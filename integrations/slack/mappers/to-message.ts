027b78eb469f82658e2654be865906c04690a99f
Hassan_Wari <thebigstormtech@gmail.com>
2024-10-02 09:20:52 +0300

    feat(integration-templates): add slack send message action

---
+++ integrations/slack/mappers/to-message.ts
@@ -0,0 +1,30 @@
+import type { SendMesssageOutput } from '../../models';
+import type { SlackResponse, SlackSuccessResponse } from '../types';
+
+function isSlackSuccessResponse(response: SlackResponse): response is SlackSuccessResponse {
+    return response.ok === true;
+}
+
+export function toMessage(response: SlackResponse): SendMesssageOutput {
+    if (isSlackSuccessResponse(response)) {
+        return {
+            ok: response.ok,
+            channel: response.channel,
+            ts: response.ts,
+            message: response.message.text,
+            warning: response.warning,
+            error: undefined,
+            raw_json: JSON.stringify(response)
+        };
+    } else {
+        return {
+            ok: response.ok,
+            channel: undefined,
+            ts: undefined,
+            message: undefined,
+            warning: undefined,
+            error: response.error,
+            raw_json: JSON.stringify(response)
+        };
+    }
+}
