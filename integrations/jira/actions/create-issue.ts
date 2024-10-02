90ca2e57620328efd9b461f916c6b1cd4bf65f70
Hassan_Wari <thebigstormtech@gmail.com>
2024-10-02 17:29:35 +0300

    feat(integration-templates): jira templates

---
+++ integrations/jira/actions/create-issue.ts
@@ -0,0 +1,41 @@
+import type { NangoAction, CreateIssueInput, CreateIssueOutput, ProxyConfiguration } from '../../models';
+import { toJiraIssue } from '../mappers/toJiraIssue.js';
+import { getCloudData } from '../helpers/get-cloud-data.js';
+
+/**
+ * This function handles the creation of an issue in Jira via the Nango action.
+ * It validates the input issue data, maps it to the appropriate Jira issue structure,
+ * and sends a request to create the issue in the Jira API.
+ * For detailed endpoint documentation, refer to:
+ *
+ * @param {NangoAction} nango - The Nango action instance to handle API requests.
+ * @param {CreateIssueInput} input - The issue data input that will be sent to Jira.
+ * @throws {nango.ActionError} - Throws an error if the input is missing or lacks required fields.
+ * @returns {Promise<CreateIssueOutput>} - Returns the created issue object from Jira.
+ */
+export default async function runAction(nango: NangoAction, input: CreateIssueInput): Promise<CreateIssueOutput> {
+    // Validate input fields: summary, issueType, and project are required
+    if (!input || !input.summary || !input.issueType || !input.project) {
+        throw new nango.ActionError({
+            message: `Required fields (summary, issueType, project) are missing. Received: ${JSON.stringify(input)}`
+        });
+    }
+
+    const cloud = await getCloudData(nango);
+
+    const jiraIssue = toJiraIssue(input);
+
+    const config: ProxyConfiguration = {
+        endpoint: `/ex/jira/${cloud.cloudId}/rest/api/3/issue`,
+        headers: {
+            'X-Atlassian-Token': 'no-check'
+        },
+        data: jiraIssue,
+        retries: 10
+    };
+
+    //https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issues/#api-rest-api-3-issue-post
+    const response = await nango.post(config);
+
+    return response.data;
+}
---
+++ integrations/jira/syncs/issue-types.ts
@@ -0,0 +1,41 @@
+import type { NangoSync, JiraIssueMetadata, ProxyConfiguration } from '../../models';
+import type { JiraIssueType } from '../types';
+import { toIssueTypes } from '../mappers/toIssueTypes.js';
+import { getCloudData } from '../helpers/get-cloud-data.js';
+
+/**
+ * Fetches and processes Jira issue types data for a specific project.
+ *
+ * @param {NangoSync} nango - The NangoSync instance for handling synchronization tasks.
+ */
+export default async function fetchData(nango: NangoSync) {
+    const cloud = await getCloudData(nango);
+
+    const metadata = await nango.getMetadata<JiraIssueMetadata>();
+
+    if (metadata && metadata.projectIdsToSync && metadata.projectIdsToSync.length > 0) {
+        for (const project of metadata.projectIdsToSync) {
+            const projectId = project.id;
+            const config: ProxyConfiguration = {
+                endpoint: `/ex/jira/${cloud.cloudId}/rest/api/3/issuetype/project`,
+                params: {
+                    projectId: Number(projectId)
+                },
+                headers: {
+                    'X-Atlassian-Token': 'no-check'
+                },
+                retries: 10
+            };
+
+            //https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issue-types/#api-rest-api-3-issuetype-project-get
+            const issueTypeResponse = await nango.get<JiraIssueType[]>(config);
+            const issueTypes = toIssueTypes(issueTypeResponse.data, projectId);
+            if (issueTypes.length > 0) {
+                await nango.batchSave(issueTypes, 'IssueType');
+            }
+        }
+    } else {
+        await nango.log('No IssueTypes to sync');
+        return;
+    }
+}
