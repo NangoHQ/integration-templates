2748d63dce4b2deaa7374839e2c9703bed47c1fe
Khaliq <khaliqgant@gmail.com>
2024-10-02 22:30:57 +0300

    deals groundwork

---
+++ integrations/hubspot/actions/fetch-properties.ts
@@ -0,0 +1,19 @@
+import type { NangoAction, ProxyConfiguration, PropertyResponse, InputProperty } from '../../models';
+
+export default async function runAction(nango: NangoAction, input: InputProperty): Promise<PropertyResponse> {
+    if (!input.name) {
+        throw new nango.ActionError({
+            message: 'An object name must be passed in to look up the properties'
+        });
+    }
+
+    const config: ProxyConfiguration = {
+        // https://developers.hubspot.com/docs/api/crm/deals
+        endpoint: `crm/v3/properties/${input.name}`,
+        retries: 10
+    };
+    const response = await nango.get(config);
+
+    return response.data
+}
+
