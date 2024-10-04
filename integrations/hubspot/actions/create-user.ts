01dbf64b085270c38217d98eed21d0070897ef36
Khaliq <khaliq@nango.dev>
2024-10-04 22:58:45 +0300

    feat(hubspot-user-operations): [nan-1860] hubspot user operations (#34)
    ## Describe your changes
    Add hubspot user operations
    - create user
    - delete user
    - fetch roles
    - change user role
    
    ## Issue ticket number and link
    NAN-1860
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [x] I added tests, otherwise the reason is:
    - [x] External API requests have `retries`
    - [x] Pagination is used where appropriate
    - [ ] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [ ] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    - [ ] If the sync is a `full` sync then `track_deletes: true` is set

---
+++ integrations/hubspot/actions/create-user.ts
@@ -0,0 +1,19 @@
+import type { NangoAction, ProxyConfiguration, CreateUser, CreatedUser } from '../../models';
+
+export default async function runAction(nango: NangoAction, input: CreateUser): Promise<CreatedUser> {
+    if (!input.email) {
+        throw new nango.ActionError({
+            message: 'Email is required'
+        });
+    }
+
+    const config: ProxyConfiguration = {
+        // https://developers.hubspot.com/docs/api/settings/user-provisioning
+        endpoint: `/settings/v3/users`,
+        data: input,
+        retries: 10
+    };
+    const response = await nango.post(config);
+
+    return response.data;
+}
--- integrations/xero/tests/xero-create-payment.test.ts
+++ integrations/xero/tests/xero-create-payment.test.ts
@@ -1,19 +1,19 @@
-import { vi, expect, it, describe } from 'vitest';
+import { vi, expect, it, describe } from "vitest";
 
-import runAction from '../actions/create-payment.js';
+import runAction from "../actions/create-payment.js";
 
-describe('xero create-payment tests', () => {
-    const nangoMock = new global.vitest.NangoActionMock({
-        dirname: __dirname,
-        name: 'create-payment',
-        Model: 'PaymentActionResponse'
-    });
+describe("xero create-payment tests", () => {
+  const nangoMock = new global.vitest.NangoActionMock({ 
+      dirname: __dirname,
+      name: "create-payment",
+      Model: "PaymentActionResponse"
+  });
 
-    it('should output the action output that is expected', async () => {
-        const input = await nangoMock.getInput();
-        const response = await runAction(nangoMock, input);
-        const output = await nangoMock.getOutput();
+  it('should output the action output that is expected', async () => {
+      const input = await nangoMock.getInput();
+      const response = await runAction(nangoMock, input);
+      const output = await nangoMock.getOutput();
 
-        expect(response).toEqual(output);
-    });
+      expect(response).toEqual(output);
+  });
 });
