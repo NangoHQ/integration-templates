dea4059e5b9e08e28c28629e968bf1159545b067
Khaliq <khaliqgant@gmail.com>
2024-10-09 00:49:33 +0100

    [nan-1848] fixes and update zod types

--- integrations/hubspot/syncs/associations.ts
+++
@@ -1,7 +0,0 @@
-import type { NangoSync, Association } from '../../models';
-
-export default async function fetchData(nango: NangoSync): Promise<void> {
-    // Integration code goes here
-    // to save data use await nango.batchSave
-    // to delete data use await nango.batchDelete
-}
