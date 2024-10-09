d51da71ba6c3995fc53dc4d12a719c7c25366fc1
Hassan_Wari <thebigstormtech@gmail.com>
2024-10-09 12:20:05 +0300

    improve

--- integrations/google-mail/syncs/emails.ts
+++ integrations/google-mail/syncs/emails.ts
@@ -82,6 +82,7 @@ function mapEmail(messageDetail: Schema$Message, headers: Record<string, any>): 
 
     processParts(parts, bodyObj, attachments);
 
+
     return {
         id: messageDetail.id,
         sender: headers['From'],
