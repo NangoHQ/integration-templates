d58a3a8ddcca05919b61f1a93ddff8dff7c4803e
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2024-10-09 16:24:03 +0300

    feat(integrations): add outlook templates (#43)
    ## Describe your changes
    
    - Add action and sync for outlook
    
    ## Issue ticket number and link
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is:
    - [x] External API requests have `retries`
    - [x] Pagination is used where appropriate
    - [x] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [x] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [x] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    - [x] If the sync is a `full` sync then `track_deletes: true` is set
    
    ---------
    
    Co-authored-by: Khaliq <khaliqgant@gmail.com>

---
+++ integrations/outlook/syncs/emails.ts
@@ -0,0 +1,74 @@
+import type { NangoSync, OutlookEmail, OptionalBackfillSetting, Attachments, ProxyConfiguration } from '../../models';
+import type { OutlookMessage, Attachment } from '../types';
+
+// 1 year ago
+const DEFAULT_BACKFILL_MS = 365 * 24 * 60 * 60 * 1000;
+
+export default async function fetchData(nango: NangoSync) {
+    const metadata = await nango.getMetadata<OptionalBackfillSetting>();
+    const backfillMilliseconds = metadata?.backfillPeriodMs || DEFAULT_BACKFILL_MS;
+    const backfillPeriod = new Date(Date.now() - backfillMilliseconds);
+    const { lastSyncDate } = nango;
+    const syncDate = lastSyncDate || backfillPeriod;
+
+    const pageSize = 100;
+
+    const config: ProxyConfiguration = {
+        endpoint: '/v1.0/me/messages',
+        params: {
+            $filter: `receivedDateTime ge ${syncDate.toISOString()}`,
+            $select: 'id,from,toRecipients,receivedDateTime,subject,attachments,conversationId,body'
+        },
+        paginate: {
+            type: 'link',
+            limit_name_in_request: '$top',
+            response_path: 'value',
+            link_path_in_response_body: '@odata.nextLink',
+            limit: pageSize
+        },
+        retries: 10
+    };
+
+    // https://learn.microsoft.com/en-us/graph/api/user-list-messages?view=graph-rest-1.0&tabs=http#example-1-list-all-messages
+    for await (const messageList of nango.paginate<OutlookMessage>(config)) {
+        const emails: OutlookEmail[] = messageList.map((message: OutlookMessage) => {
+            const headers = extractHeaders(message);
+            return mapEmail(message, headers);
+        });
+
+        await nango.batchSave(emails, 'OutlookEmail');
+    }
+}
+
+function extractHeaders(message: OutlookMessage): Record<string, any> {
+    return {
+        From: message.from?.address,
+        To: message.toRecipients?.map((recipient) => recipient.emailAddress.address).join(', '),
+        Subject: message.subject
+    };
+}
+
+function processParts(attachments: Attachment[]): Attachments[] {
+    return attachments.map((attachment) => ({
+        attachmentId: attachment.id,
+        mimeType: attachment.contentType,
+        filename: attachment.name,
+        size: attachment.size
+    }));
+}
+
+function mapEmail(messageDetail: OutlookMessage, headers: Record<string, any>): OutlookEmail {
+    const bodyObj = { body: messageDetail.body?.content || '' };
+    const attachments: Attachments[] = messageDetail.attachments ? processParts(messageDetail.attachments) : [];
+
+    return {
+        id: messageDetail.id,
+        sender: headers['From'],
+        recipients: headers['To'],
+        date: new Date(messageDetail.receivedDateTime).toISOString(),
+        subject: headers['Subject'],
+        body: bodyObj.body,
+        attachments,
+        threadId: messageDetail.conversationId
+    };
+}
---
+++ integrations/outlook/types.ts
@@ -0,0 +1,30 @@
+export interface OutlookMessage {
+    id: string;
+    from: EmailAddress;
+    toRecipients: Recipient[];
+    receivedDateTime: string;
+    subject: string;
+    attachments: Attachment[];
+    conversationId: string;
+    body: BodyItem;
+}
+
+interface EmailAddress {
+    address: string;
+}
+interface BodyItem {
+    content: string;
+    contentType: string;
+}
+export interface Attachment {
+    contentType: string;
+    id: string;
+    isInline: boolean;
+    lastModifiedDateTime: string;
+    name: string;
+    size: number;
+}
+
+export interface Recipient {
+    emailAddress: EmailAddress;
+}
