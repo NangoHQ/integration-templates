6ddf4ffd12f09cea906233d353a30d70b7c4cd7a
arealesramirez <arealesramirez8606@gmail.com>
2024-10-14 13:20:52 -0500

    adding documentation

--- integrations/box/actions/create-user.ts
+++ integrations/box/actions/create-user.ts
@@ -1,5 +1,14 @@
 import type { NangoAction, CreateUser, CreatedUser } from '../../models';
 
+/**
+ * Validates the input for the create user action.
+ * Throws an error if the required fields are missing:
+ * - 'login' is required unless 'is_platform_access_only' is set to true.
+ * - 'name' is always required.
+ *
+ * For more info about required fields, check the documentation:
+ * https://developer.box.com/reference/post-users/
+ */
 function validateInput(nango: NangoAction, input: CreateUser): void {
     if (!input.login && input.is_platform_access_only !== true) {
         throw new nango.ActionError({
@@ -14,6 +23,10 @@ function validateInput(nango: NangoAction, input: CreateUser): void {
     }
 }
 
+/**
+ * Executes the create user action by validating input, constructing the request configuration,
+ * and making the API call to create a new user.
+ */
 export default async function runAction(nango: NangoAction, input: CreateUser): Promise<CreatedUser> {
     validateInput(nango, input);
 
--- integrations/box/actions/delete-user.ts
+++ integrations/box/actions/delete-user.ts
@@ -1,12 +1,21 @@
 import type { NangoAction, SuccessResponse, DeleteUser } from '../../models';
 
-export default async function runAction(nango: NangoAction, input: DeleteUser): Promise<SuccessResponse> {
+/**
+ * Validates the input for the delete user action.
+ * Throws an error if the input is missing or if the required 'id' field is not provided.
+ */
+function validateInput(nango: NangoAction, input: DeleteUser): void {
     if (!input || !input.id) {
         throw new nango.ActionError({
             message: 'Id is required'
         });
     }
+}
 
+/**
+ * Constructs the API endpoint URL for deleting a user based on the provided input.
+ */
+function getEndpoint(input: DeleteUser): string {
     const endpoint = `/2.0/users/${input.id}`;
 
     const queryParams = Object.entries({
@@ -17,11 +26,20 @@ export default async function runAction(nango: NangoAction, input: DeleteUser): 
         .join('&');
 
     const params = queryParams ? `&${queryParams}` : '';
-    const urlWithParams = `${endpoint}${params}`;
+
+    return `${endpoint}${params}`;
+}
+
+/**
+ * Executes the delete user action by validating input, constructing the endpoint,
+ * and making the API call to Box to delete the specified user.
+ */
+export default async function runAction(nango: NangoAction, input: DeleteUser): Promise<SuccessResponse> {
+    validateInput(nango, input);
 
     const config = {
         // https://developer.box.com/reference/delete-users-id/
-        endpoint: urlWithParams,
+        endpoint: getEndpoint(input),
         retries: 10
     };
 
