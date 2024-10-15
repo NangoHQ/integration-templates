0c10285e731d2e0b3a332ea4b445b97ffd962e26
arealesramirez <arealesramirez8606@gmail.com>
2024-10-15 10:36:58 -0500

    Add DocuSign users sync

---
+++ integrations/docusign/types.ts
@@ -0,0 +1,21 @@
+export interface DocuSignUser {
+    userName: string;
+    userId: string;
+    userType: string;
+    isAdmin: string;
+    isAlternateAdmin: string;
+    userStatus: string;
+    uri: string;
+    email: string;
+    title: string;
+    createdDateTime: string;
+    userAddedToAccountDateTime: string;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    suffixName: string;
+    jobTitle: string;
+    company: string;
+    permissionProfileId: string;
+    permissionProfileName: string;
+}
