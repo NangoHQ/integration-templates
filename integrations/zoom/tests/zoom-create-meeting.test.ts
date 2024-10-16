67d3f0fdbd376a3c70a6f261dde9d1fba8536545
Khaliq <khaliq@nango.dev>
2024-10-16 15:38:52 +0200

    feat(zoom-meetings): [nan-1899] zoom operations (#54)
    ## Describe your changes
    Add in zoom user operations and meeting operations
    
    ## Issue ticket number and link
    NAN-1899
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is:
    - [ ] External API requests have `retries`
    - [ ] Pagination is used where appropriate
    - [ ] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [ ] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    - [ ] If the sync is a `full` sync then `track_deletes: true` is set

---
+++ integrations/zoom/tests/zoom-create-meeting.test.ts
@@ -0,0 +1,19 @@
+import { vi, expect, it, describe } from "vitest";
+
+import runAction from "../actions/create-meeting.js";
+
+describe("zoom create-meeting tests", () => {
+  const nangoMock = new global.vitest.NangoActionMock({ 
+      dirname: __dirname,
+      name: "create-meeting",
+      Model: "Meeting"
+  });
+
+  it('should output the action output that is expected', async () => {
+      const input = await nangoMock.getInput();
+      const response = await runAction(nangoMock, input);
+      const output = await nangoMock.getOutput();
+
+      expect(response).toEqual(output);
+  });
+});
