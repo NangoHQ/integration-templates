8e42930a80f83cad598769521207dc43cc314b87
Khaliq <khaliq@nango.dev>
2024-10-16 15:47:36 +0200

    feat(jira): [nan-1860] add in user operations (#53)
    ## Describe your changes
    Add in JIRA User operations using the `jira-basic` integration which is
    an API key
    
    ## Issue ticket number and link
    NAN-1860
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [x] I added tests, otherwise the reason is:
    - [x] External API requests have `retries`
    - [x] Pagination is used where appropriate
    - [x] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [ ] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    - [x] If the sync is a `full` sync then `track_deletes: true` is set

---
+++ integrations/jira-basic/actions/delete-user.ts
@@ -0,0 +1,24 @@
+import type { NangoAction, ProxyConfiguration, SuccessResponse, IdEntity } from '../../models';
+
+export default async function runAction(nango: NangoAction, input: IdEntity): Promise<SuccessResponse> {
+    if (!input.id) {
+        throw new nango.ActionError({
+            message: 'Id is required to delete a user'
+        });
+    }
+
+    const config: ProxyConfiguration = {
+        // https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-users/#api-rest-api-3-user-delete
+        endpoint: `/rest/api/3/user`,
+        params: {
+            accountId: input.id
+        },
+        retries: 10
+    };
+
+    await nango.delete(config);
+
+    return {
+        success: true
+    };
+}
---
+++ integrations/jira-basic/types.ts
@@ -0,0 +1,41 @@
+export interface JiraUser {
+    self: string;
+    accountId: string;
+    accountType: string;
+    emailAddress?: string;
+    avatarUrls: {
+        '48x48': string;
+        '24x24': string;
+        '16x16': string;
+        '32x32': string;
+    };
+    displayName: string;
+    active: boolean;
+    locale: string;
+}
+
+export interface JiraCreatedUser {
+    self: string;
+    accountId: string;
+    accountType: string;
+    emailAddress: string;
+    avatarUrls: {
+        '48x48': string;
+        '24x24': string;
+        '16x16': string;
+        '32x32': string;
+    };
+    displayName: string;
+    active: boolean;
+    timeZone: string;
+    locale: string;
+    groups: {
+        size: number;
+        items: any[]; // If you have a specific structure for items, replace 'any' with the proper type
+    };
+    applicationRoles: {
+        size: number;
+        items: any[]; // Similar to groups, specify the structure of items if known
+    };
+    expand: string;
+}
