d92982c2018cb2aecfa6463b7573b2d27a859d70
Khaliq <khaliqgant@gmail.com>
2024-10-17 13:34:56 +0200

    minor cleanup

--- integrations/docusign/syncs/users.ts
+++ integrations/docusign/syncs/users.ts
@@ -2,9 +2,6 @@ import type { NangoSync, ProxyConfiguration, User } from '../../models';
 import { getRequestInfo } from '../helpers/get-request-info.js';
 import type { DocuSignUser } from '../types';
 
-// Based on the api: https://developers.docusign.com/docs/esign-rest-api/reference/users/users/list/ Valid values: 1 to 100
-const LIMIT = 100;
-
 /**
  * Fetches user data from the DocuSign API and saves it in batches.
  */
@@ -14,13 +11,17 @@ export default async function fetchData(nango: NangoSync) {
 
     const proxyConfiguration: ProxyConfiguration = {
         baseUrlOverride: baseUri,
-        endpoint: `/restapi/v2.1/accounts/${accountId}/users?status=Active,ActivationRequired,ActivationSent`, // excluding Closed (soft delete) and Disabled statuses
+        // https://developers.docusign.com/docs/esign-rest-api/reference/users/users/list/ Valid values: 1 to 100
+        endpoint: `/restapi/v2.1/accounts/${accountId}/users`, // excluding Closed (soft delete) and Disabled statuses
+        params: {
+            status: 'Active,ActivationRequired,ActivationSent'
+        },
         paginate: {
             type: 'offset',
             offset_name_in_request: 'start_position',
             limit_name_in_request: 'count',
             response_path: 'users',
-            limit: LIMIT
+            limit: 100
         }
     };
 
