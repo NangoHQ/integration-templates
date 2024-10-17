ee3b153b5f4f5bc08cf573c1a573e0d7f5016ddb
Khaliq <khaliqgant@gmail.com>
2024-10-17 17:05:41 +0200

    symlink

--- integrations/gusto/types.ts
+++ integrations/gusto/types.ts
@@ -91,67 +91,67 @@ interface CustomField {
 }
 
 export interface EmployeeResponse {
-  uuid: string;
-  first_name: string;
-  middle_initial: string | null;
-  last_name: string;
-  email: string;
-  company_uuid: string;
-  manager_uuid: string;
-  version: string;
-  department: string;
-  department_uuid: string;
-  terminated: boolean;
-  two_percent_shareholder: boolean;
-  onboarded: boolean;
-  onboarding_status: string;
-  jobs: Job[];
-  eligible_paid_time_off: PaidTimeOff[];
-  terminations: any[];
-  garnishments: any[];
-  custom_fields?: CustomField[];
-  date_of_birth: string;
-  has_ssn: boolean;
-  ssn: string;
-  phone: string;
-  preferred_first_name: string;
-  work_email: string;
+    uuid: string;
+    first_name: string;
+    middle_initial: string | null;
+    last_name: string;
+    email: string;
+    company_uuid: string;
+    manager_uuid: string;
+    version: string;
+    department: string;
+    department_uuid: string;
+    terminated: boolean;
+    two_percent_shareholder: boolean;
+    onboarded: boolean;
+    onboarding_status: string;
+    jobs: Job[];
+    eligible_paid_time_off: PaidTimeOff[];
+    terminations: any[];
+    garnishments: any[];
+    custom_fields?: CustomField[];
+    date_of_birth: string;
+    has_ssn: boolean;
+    ssn: string;
+    phone: string;
+    preferred_first_name: string;
+    work_email: string;
 }
 
 interface Job {
-  uuid: string;
-  version: string;
-  employee_uuid: string;
-  current_compensation_uuid: string;
-  payment_unit: string;
-  primary: boolean;
-  title: string;
-  compensations: Compensation[];
-  rate: string;
-  hire_date: string;
+    uuid: string;
+    version: string;
+    employee_uuid: string;
+    current_compensation_uuid: string;
+    payment_unit: string;
+    primary: boolean;
+    title: string;
+    compensations: Compensation[];
+    rate: string;
+    hire_date: string;
 }
 
 interface Compensation {
-  uuid: string;
-  version: string;
-  payment_unit: string;
-  flsa_status: string;
-  job_uuid: string;
-  effective_date: string;
-  rate: string;
-  adjust_for_minimum_wage: boolean;
-  minimum_wages: any[];
+    uuid: string;
+    version: string;
+    payment_unit: string;
+    flsa_status: string;
+    job_uuid: string;
+    effective_date: string;
+    rate: string;
+    adjust_for_minimum_wage: boolean;
+    minimum_wages: any[];
 }
 
 interface PaidTimeOff {
-  name: string;
-  policy_name: string;
-  policy_uuid: string;
-  accrual_unit: string;
-  accrual_rate: string;
-  accrual_method?: string;
-  accrual_period: string;
-  accrual_balance: string;
-  maximum_accrual_balance: string;
-  paid_at_termination: boolean;
+    name: string;
+    policy_name: string;
+    policy_uuid: string;
+    accrual_unit: string;
+    accrual_rate: string;
+    accrual_method?: string;
+    accrual_period: string;
+    accrual_balance: string;
+    maximum_accrual_balance: string;
+    paid_at_termination: boolean;
 }
