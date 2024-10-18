c1d71586a5190c7894687bda8f03e19c4ec2f60c
arealesramirez <arealesramirez8606@gmail.com>
2024-10-18 18:07:15 -0500

    updated users sync for freshdesk

--- integrations/freshdesk/syncs/users.ts
+++ integrations/freshdesk/syncs/users.ts
@@ -1,50 +1,53 @@
-import type {
-    NangoSync,
-    ProxyConfiguration
-    // User
-} from '../../models';
+import type { NangoSync, ProxyConfiguration, User } from '../../models';
+import type { FreshdeskUser } from '../types';
 
-// TODO: docs
+/**
+ * Fetches user data from Freshdesk and saves it in batches.
+ *
+ * Retrieves contacts from the Freshdesk API using pagination. It accumulates the total number of records fetched
+ * and logs the progress for each batch of users saved. The function continues to fetch data until all records have been retrieved.
+ *
+ * List all contacts Freshdesk API docs: https://developer.freshdesk.com/api/#list_all_contacts
+ *
+ */
+// https://developer.freshdesk.com/api/#contacts
 export default async function fetchData(nango: NangoSync): Promise<void> {
-    // let totalRecords = 0;
+    let totalRecords = 0;
 
     const proxyConfiguration: ProxyConfiguration = {
-        endpoint: '/api/v2/contacts.json', // TODO: verify if we need to filter out deleted
-        retries: 10
-        // params: {
-        //     page: 1
-        // },
-        // paginate: {
-        //     type: 'link',
-        //     limit_name_in_request: 'page',
-        //     link_rel_in_response_header: 'page',
-        //     limit: 100
-        // }
+        endpoint: '/api/v2/contacts',
+        retries: 10,
+        paginate: {
+            type: 'link',
+            limit_name_in_request: 'per_page',
+            link_rel_in_response_header: 'next',
+            limit: 1
+        }
     };
 
-    const response = await nango.get<any>(proxyConfiguration);
-    console.log('response', response.data);
+    for await (const freshdeskUsers of nango.paginate(proxyConfiguration)) {
+        const batchSize: number = freshdeskUsers.length || 0;
+        totalRecords += batchSize;
 
-    // for await (const freshdeskUsers of nango.paginate(proxyConfiguration)) {
-    //     console.log('freshdeskUsers', freshdeskUsers);
+        const users: User[] = freshdeskUsers.map(mapUser) || [];
 
-    //     const batchSize: number = freshdeskUsers.length || 0;
-    //     totalRecords += batchSize;
+        await nango.log(`Saving batch of ${batchSize} users (total users: ${totalRecords})`);
 
-    //     const users: User[] = freshdeskUsers.map(mapUser) || [];
-
-    //     await nango.log(`Saving batch of ${batchSize} users (total users: ${totalRecords})`);
-
-    //     await nango.batchSave(users, 'User');
-    // }
+        await nango.batchSave(users, 'User');
+    }
 }
 
-// // TODO: docs
-// function mapUser(user: any): User {
-//     return {
-//         id: user.userId,
-//         email: user.email,
-//         firstName: user.firstName,
-//         lastName: user.lastName
-//     };
-// }
+/**
+ * Maps a Freshdesk user object to a Nango User object.
+ */
+
+function mapUser(user: FreshdeskUser): User {
+    const [firstName = '', lastName = ''] = (user?.name ?? '').split(' ');
+
+    return {
+        id: user.id.toString(),
+        email: user.email,
+        firstName,
+        lastName
+    };
+}
