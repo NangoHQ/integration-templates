620bbfd8754dc6b242f0d2d0e0e4cda7fcbfbd69
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2024-10-22 10:35:37 +0300

    feat(integrations): add okta integrations (#65)
    ## Describe your changes
    Add okta actions:
    
    - create-user
    - add-group
    - add-user-group
    - remove-user-group
    
    ## Issue ticket number and link
    
    [EXT-190](https://linear.app/nango/issue/EXT-190/basic-user-and-group-actions-with-okta)
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [x] I added tests, otherwise the reason is:
    - [x] External API requests have `retries`
    - [ ] Pagination is used where appropriate
    - [ ] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [x] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    - [ ] If the sync is a `full` sync then `track_deletes: true` is set

---
+++ integrations/okta/mappers/toGroup.ts
@@ -0,0 +1,50 @@
+import type { OktaUserGroupProfile, OktaActiveDirectoryGroupProfile, OktaGroup, CreateOktaGroup } from '../types';
+import type { Group, OktaAddGroup } from '../../models';
+
+export function toGroup(group: OktaGroup): Group {
+    let profile: OktaUserGroupProfile | OktaActiveDirectoryGroupProfile | null = null;
+
+    if (group.type === 'OKTA_GROUP' || group.type === 'BUILT_IN' || group.type === 'APP_GROUP') {
+        if ('dn' in group.profile) {
+            profile = {
+                description: group.profile.description,
+                dn: group.profile.dn,
+                externalId: group.profile.externalId,
+                name: group.profile.name,
+                samAccountName: group.profile.samAccountName,
+                windowsDomainQualifiedName: group.profile.windowsDomainQualifiedName
+            };
+        } else {
+            profile = {
+                description: group.profile.description || null,
+                name: group.profile.name
+            };
+        }
+    }
+
+    return {
+        id: group.id,
+        created: group.created,
+        lastMembershipUpdated: group.lastMembershipUpdated,
+        lastUpdated: group.lastUpdated,
+        objectClass: group.objectClass,
+        type: group.type,
+        profile: profile!
+    };
+}
+
+export function createGroup(group: OktaAddGroup): Partial<CreateOktaGroup> {
+    const oktaGroup: Partial<CreateOktaGroup> = {
+        profile: {}
+    };
+
+    if (group.name) {
+        oktaGroup.profile!.name = group.name;
+    }
+
+    if (group.description) {
+        oktaGroup.profile!.description = group.description;
+    }
+
+    return oktaGroup;
+}
