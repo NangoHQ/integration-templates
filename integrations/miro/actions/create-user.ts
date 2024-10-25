4c92082e5086e63c66a5035988cf3a8e12651234
arealesramirez <arealesramirez8606@gmail.com>
2024-10-25 08:40:28 -0500

    defining schema for create-user action in miro integration

---
+++ integrations/miro/actions/create-user.ts
@@ -0,0 +1,5 @@
+import type { NangoAction, User, CreateUser } from '../../models';
+
+export default async function runAction(nango: NangoAction, input: CreateUser): Promise<User> {
+    // Integration code goes here
+}
