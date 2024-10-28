ecd9bf02278343d877c0cfc9c342e8c86bc20c4d
arealesramirez <arealesramirez8606@gmail.com>
2024-10-28 08:41:04 -0500

    work in progress: adding create-user for keeper integration

---
+++ integrations/keeper/actions/create-user.ts
@@ -0,0 +1,5 @@
+import type { NangoAction, User, KeeperCreateUser } from '../../models';
+
+export default async function runAction(nango: NangoAction, input: KeeperCreateUser): Promise<User> {
+    // Integration code goes here
+}
