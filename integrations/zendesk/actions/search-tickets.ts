deff79d1fe635fb13dec55623f6f054892011f8c
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2024-10-30 23:55:38 +0300

    feat(integrations): add zendesk search action  (#86)
    ## Describe your changes
    
    ## Issue ticket number and link
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is:
    - [x] External API requests have `retries`
    - [ ] Pagination is used where appropriate
    - [ ] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [x] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    - [ ] If the sync is a `full` sync then `track_deletes: true` is set

---
+++ integrations/zendesk/actions/search-tickets.ts
@@ -0,0 +1,71 @@
+import type { NangoAction, SearchTicketInput, SearchTicketOutput, SearchTicket, ProxyConfiguration } from '../../models';
+import { toTicket } from '../mappers/toTicket.js';
+import type { ZendeskSearchTicketsResponse, ZendeskTicket, ZendeskUser, ZendeskPaginationParams } from '../types';
+
+export default async function runAction(nango: NangoAction, input: SearchTicketInput): Promise<SearchTicketOutput> {
+    if (!input.query) {
+        throw new nango.ActionError({
+            message: 'Search query is required'
+        });
+    }
+
+    const config = {
+        endpoint: `/api/v2/search`,
+        params: {
+            query: input.query,
+            include: 'tickets(users)'
+        },
+        page_size: 100
+    };
+
+    const tickets: SearchTicket[] = [];
+
+    // https://developer.zendesk.com/api-reference/ticketing/ticket-management/search/#query-basics
+    for await (const { tickets: pagedTickets, users: pagedUsers } of paginate(nango, config)) {
+        const mappedTickets = await Promise.all(pagedTickets.map((ticket) => toTicket(ticket, pagedUsers)));
+        tickets.push(...mappedTickets);
+    }
+
+    return {
+        tickets
+    };
+}
+
+async function* paginate(
+    nango: NangoAction,
+    { endpoint, params, page_size }: ZendeskPaginationParams
+): AsyncGenerator<{ tickets: ZendeskTicket[]; users: ZendeskUser[] }, void, undefined> {
+    let nextPageLink: string | null = endpoint;
+
+    while (nextPageLink) {
+        const configParams = {
+            per_page: page_size,
+            query: params.query,
+            include: params.include
+        };
+
+        const config: ProxyConfiguration = {
+            endpoint: nextPageLink,
+            params: configParams,
+            retries: 10
+        };
+
+        const response = await nango.get<ZendeskSearchTicketsResponse>(config);
+
+        const tickets: ZendeskTicket[] = response.data.results || [];
+        const users: ZendeskUser[] = response.data.users || [];
+
+        const moreDataAvailable = !!response.data.next_page;
+
+        yield { tickets, users };
+
+        if (!moreDataAvailable) break;
+
+        nextPageLink = response.data.next_page || null;
+
+        if (nextPageLink) {
+            const urlParts = new URL(nextPageLink);
+            nextPageLink = `${urlParts.pathname}${urlParts.search}`;
+        }
+    }
+}
---
+++ integrations/zendesk/mappers/toTicket.ts
@@ -0,0 +1,34 @@
+import type { ZendeskUser, ZendeskTicket } from '../types';
+import type { SearchTicket } from '../../models';
+
+export function toTicket(ticket: ZendeskTicket, users: ZendeskUser[]): SearchTicket {
+    const requester = users.find((user) => user.id === ticket.requester_id);
+    const assignee = users.find((user) => user.id === ticket.assignee_id);
+
+    if (!requester) {
+        throw new Error(`Requester with ID ${ticket.requester_id} not found`);
+    }
+
+    if (!assignee) {
+        throw new Error(`Assignee with ID ${ticket.assignee_id} not found`);
+    }
+
+    return {
+        id: ticket.id.toString(),
+        url: ticket.url,
+        external_id: ticket.external_id,
+        requester_id: requester.id.toString(),
+        requester_name: requester.name,
+        assignee_id: assignee.id.toString(),
+        assignee_name: assignee.name,
+        assignee_avatar: assignee.photo ? assignee.photo.content_url : null,
+        status: ticket.status,
+        created_at: new Date(ticket.created_at).toISOString(),
+        updated_at: new Date(ticket.updated_at).toISOString(),
+        is_public: ticket.is_public,
+        subject: ticket.subject,
+        description: ticket.description,
+        priority: ticket.priority,
+        tags: ticket.tags
+    };
+}
