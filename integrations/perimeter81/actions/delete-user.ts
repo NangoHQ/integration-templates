5d68ba94b591071831e9fc4906ec0a0fa38954fb
Andres Reales <arealesramirez8606@gmail.com>
2024-10-31 04:22:18 -0500

    feat(perimeter81): Add users endpoints to Perimeter81 integration (#81)
    ## Describe your changes
    Add Perimeter81 integration with the following endpoints:
    
    ### Syncs
    - users
    
    ### Actions
    - create-user
    - delete-user
    
    **Important Note: No fixtures, nor tests/mocks were generated. I could
    not test any of the syncs and actions added to the Perimeter81
    integration as it requires an enterprise account, which is not
    accessible to me. Hence, once a connection is setup in Nango, these
    endpoints need to be tested and validated**
    
    ## Issue ticket number and link
    N/A
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [ ] I added tests, otherwise the reason is: I could not test any of
    the syncs and actions added to the Perimeter81 integration as it
    requires an enterprise account, which is not accessible to me. Hence,
    once a connection is setup in Nango, these endpoints need to be tested
    and validated
    - [X] External API requests have `retries`
    - [X] Pagination is used where appropriate
    - [X] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [ ] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    - [X] If the sync is a `full` sync then `track_deletes: true` is set
    
    ---------
    
    Co-authored-by: Khaliq <khaliqgant@gmail.com>
    Co-authored-by: Khaliq <khaliq@nango.dev>

---
+++ integrations/perimeter81/actions/delete-user.ts
@@ -0,0 +1,46 @@
+import type { NangoAction, ProxyConfiguration, SuccessResponse, IdEntity } from '../../models';
+import { idEntitySchema } from '../schema.zod.js';
+
+/**
+ * Deletes a Perimeter81 user.
+ *
+ * This function validates the input against the defined schema and constructs a request
+ * to the Perimeter81 API to delete a user by their ID. If the input is invalid,
+ * it logs the errors and throws an ActionError.
+ *
+ * @param {NangoAction} nango - The Nango action context, used for logging and making API requests.
+ * @param {IdEntity} input - The input data containing the ID of the user contact to be deleted
+ *
+ * @returns {Promise<SuccessResponse>} - A promise that resolves to a SuccessResponse object indicating the result of the deletion.
+ *
+ * @throws {nango.ActionError} - Throws an error if the input validation fails.
+ *
+ * For detailed endpoint documentation, refer to:
+ * https://support.perimeter81.com/docs/delete-delete-user
+ */
+export default async function runAction(nango: NangoAction, input: IdEntity): Promise<SuccessResponse> {
+    const parsedInput = idEntitySchema.safeParse(input);
+
+    if (!parsedInput.success) {
+        for (const error of parsedInput.error.errors) {
+            await nango.log(`Invalid input provided to delete a user: ${error.message} at path ${error.path.join('.')}`, { level: 'error' });
+        }
+
+        throw new nango.ActionError({
+            message: 'Invalid input provided to delete a user'
+        });
+    }
+
+    const config: ProxyConfiguration = {
+        // https://support.perimeter81.com/docs/delete-delete-user
+        endpoint: `/v1/users/${parsedInput.data.id}`,
+        retries: 10
+    };
+
+    // no body content expected for successful requests
+    await nango.delete(config);
+
+    return {
+        success: true
+    };
+}
---
+++ integrations/perimeter81/syncs/users.ts
@@ -0,0 +1,39 @@
+import type { NangoSync, ProxyConfiguration, User } from '../../models';
+import { toUser } from '../mappers/to-user.js';
+import type { Perimeter81User } from '../types';
+
+/**
+ * Fetches Perimeter81 users, maps them to Nango User objects,
+ * and saves the processed contacts using NangoSync.
+ *
+ * This function handles pagination and ensures that all contacts are fetched,
+ * transformed, and stored.
+ *
+ * For endpoint documentation, refer to:
+ * https://support.perimeter81.com/docs/get-list-users
+ *
+ * @param nango An instance of NangoSync for synchronization tasks.
+ * @returns Promise that resolves when all users are fetched and saved.
+ */
+export default async function fetchData(nango: NangoSync): Promise<void> {
+    const config: ProxyConfiguration = {
+        // https://support.perimeter81.com/docs/get-list-users
+        endpoint: '/v1/users',
+        paginate: {
+            type: 'offset',
+            offset_name_in_request: 'page',
+            offset_start_value: 1,
+            offset_calculation_method: 'per-page',
+            limit_name_in_request: 'limit',
+            response_path: 'data',
+            limit: 100
+        },
+        retries: 10
+    };
+
+    for await (const perimeter81Users of nango.paginate<Perimeter81User>(config)) {
+        const users = perimeter81Users.map(toUser);
+
+        await nango.batchSave<User>(users, 'User');
+    }
+}
