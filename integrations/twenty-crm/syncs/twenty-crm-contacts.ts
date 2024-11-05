d86f7672b5a985c05a86cc085caf0551132c111a
mungai <mungaihosea@gmail.com>
2024-11-05 15:38:20 +0300

    wip adding twenty crm syncs

---
+++ integrations/twenty-crm/syncs/twenty-crm-contacts.ts
@@ -0,0 +1,8 @@
+import type { NangoSync, Contact } from '../../models';
+
+export default async function fetchData(nango: NangoSync): Promise<void> {
+    // Integration code goes here
+    // to save data use await nango.batchSave
+    // to delete data use await nango.batchDelete
+}
+
