d38150f6b184f9f102e98f93a4e5f58c6cba801f
mungai <mungaihosea@gmail.com>
2024-11-07 02:12:46 +0300

    added dropbox file sync && fetch-file action

---
+++ integrations/dropbox/actions/fetch-file.ts
@@ -0,0 +1,39 @@
+import type { NangoAction, ProxyConfiguration } from '../../models';
+import { DropboxTemporaryDownloadLink } from '../types.js';
+
+
+export default async function runAction(nango: NangoAction, input: string): Promise<string> {
+
+    if (!input || typeof input !== 'string') {
+        throw new Error('Missing or invalid input: a file ID is required and should be a string');
+    } 
+
+    const proxyConfig: ProxyConfiguration = {
+        endpoint: `/2/files/get_temporary_link`,
+        data: {
+            path: input
+        }
+    }
+
+    const { data } = await nango.post<DropboxTemporaryDownloadLink>(proxyConfig)
+
+    if (!data.metadata.is_downloadable) {
+        throw new Error('File is not downloadable');
+    }
+
+    const config: ProxyConfiguration = {
+        // https://www.dropbox.com/developers/documentation/http/documentation#files-get_temporary_link
+        endpoint: data.link,
+        responseType: 'arraybuffer',
+        retries: 10
+    }
+    
+    const response = await nango.get(config)
+    
+    const chunks: Buffer[] = [];
+    for await (const chunk of response.data) {
+        chunks.push(chunk);
+    }
+    const buffer = Buffer.concat(chunks);
+    return buffer.toString('base64');
+}
---
+++ integrations/dropbox/syncs/files.ts
@@ -0,0 +1,64 @@
+import type { NangoSync, ProxyConfiguration } from '../../models';
+import { DropboxFile, DropboxFileList } from '../types';
+
+export default async function fetchData(nango: NangoSync): Promise<void> {
+
+    const config: ProxyConfiguration = {
+        // https://www.dropbox.com/developers/documentation/http/documentation#files-list_folder
+        endpoint: `/2/files/list_folder`,
+        retries: 10,
+        data: {
+            path: '',
+            limit: 100,
+            include_mounted_folders: true,
+            include_non_downloadable_files: false,
+            recursive: true
+        }
+    };
+
+    const {data } = await nango.post<DropboxFileList>(config);
+    const { entries } = data;
+    
+    let hasMore = data.has_more;
+    let cursor = data.cursor;
+
+    
+    const files = entries.map((entry: DropboxFile) => {
+        return {
+            id: entry.id,
+            path: entry.path_lower,
+            title: entry.name
+        };
+    });
+
+    await nango.batchSave(files, 'Document');
+
+    while (hasMore) {
+        const config: ProxyConfiguration = {
+            // https://www.dropbox.com/developers/documentation/http/documentation#sharing-list_folders-continue
+            endpoint: `/2/files/list_folder/continue`,
+            retries: 10,
+            data: {
+                cursor
+            }
+        };
+
+        const {data } = await nango.post<DropboxFileList>(config);
+        const { entries } = data;
+        
+        hasMore = data.has_more;
+        cursor = data.cursor;
+        
+        const files = entries.map((entry: DropboxFile) => {
+            return {
+                id: entry.id,
+                path: entry.path_lower,
+                title: entry.name
+            };
+        });
+
+        await nango.batchSave(files, 'Document');
+    }
+
+}
+
