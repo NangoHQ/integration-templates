0a5eed9312c5d8b2e10858e09bc2eca85e2d96b4
mungai <mungaihosea@gmail.com>
2024-11-07 13:27:25 +0300

    Added feature to delete users by id on aircall-basic

--- integrations/aircall-basic/actions/delete-user.ts
+++ integrations/aircall-basic/actions/delete-user.ts
@@ -1,21 +1,45 @@
-import type { NangoAction, ProxyConfiguration, SuccessResponse, IdEntity } from '../../models';
+import type { NangoAction, ProxyConfiguration, SuccessResponse, EmailEntity, User } from '../../models';
+import type { AircallUser } from '../types';
 
-export default async function runAction(nango: NangoAction, input: IdEntity): Promise<SuccessResponse> {
-    if (!input || !input.id) {
+export default async function runAction(nango: NangoAction, input: EmailEntity): Promise<SuccessResponse> {
+    if (!input || !input.email) {
         throw new nango.ActionError({
-            message: 'Id is required'
+            message: 'Email is required'
         });
     }
 
     const config: ProxyConfiguration = {
-        // https://developer.aircall.io/api-references/#delete-a-user
-        endpoint: `/v1/users/${input.id}`,
-        retries: 10
+        // https://developer.aircall.io/api-references/#list-all-users
+        endpoint: '/v1/users',
+        retries: 10,
+        paginate: {
+            response_path: 'users'
+        }
     };
+    
+    let foundUserId: string | undefined;
 
-    await nango.delete(config);
+    for await (const aUsers of nango.paginate<AircallUser>(config)) {
+        const matchingUser = aUsers.find((aUser) => aUser.email === input.email);
+        if (matchingUser) {
+            foundUserId = matchingUser.id.toString();
+            break;
+        }
+    }
+
+    if (!foundUserId) {
+        throw new nango.ActionError({
+            message: 'Could not find user with the specified email'
+        });
+    }
+
+    await nango.delete({
+        endpoint: `/v1/users/${foundUserId}`,
+        retries: 10
+    });
 
     return {
         success: true
-    };
+    }
+
 }
