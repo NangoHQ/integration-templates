3f22f2e69ec0a58043db6a38af6d6ebb23053d8a
mungai <mungaihosea@gmail.com>
2024-11-07 18:32:23 +0300

    added feature to remove user by email

--- integrations/freshdesk/actions/delete-user.ts
+++ integrations/freshdesk/actions/delete-user.ts
@@ -1,22 +1,51 @@
-import type { NangoAction, ProxyConfiguration, SuccessResponse, IdEntity } from '../../models';
-import { idEntitySchema } from '../schema.zod.js';
+import type { NangoAction, ProxyConfiguration, SuccessResponse, EmailEntity } from '../../models';
+import { FreshdeskAgent } from '../types';
+import { emailEntitySchema } from '../schema.zod.js';
 
-export default async function runAction(nango: NangoAction, input: IdEntity): Promise<SuccessResponse> {
-    const parsedInput = idEntitySchema.safeParse(input);
+async function fetchUserIdByEmail(nango: NangoAction, email: string): Promise<string | undefined> {
+    const proxyConfiguration: ProxyConfiguration = {
+        // https://developer.freshdesk.com/api/#list_all_agents
+        endpoint: '/api/v2/agents',
+        retries: 10,
+        paginate: {
+            type: 'link',
+            limit_name_in_request: 'per_page',
+            link_rel_in_response_header: 'next',
+            limit: 100
+        }
+    };
+
+    for await (const freshdeskUsers of nango.paginate<FreshdeskAgent>(proxyConfiguration)) {
+        const foundUser = freshdeskUsers.find(user => user.contact.email === email);
+
+        if (foundUser) {
+            return foundUser.id.toString();
+        }
+    }
+
+    return undefined;
+}
+
+export default async function runAction(nango: NangoAction, input: EmailEntity): Promise<SuccessResponse> {
+    const parsedInput = emailEntitySchema.safeParse(input);
 
     if (!parsedInput.success) {
-        for (const error of parsedInput.error.errors) {
-            await nango.log(`Invalid input provided to delete a user: ${error.message} at path ${error.path.join('.')}`, { level: 'error' });
-        }
-
         throw new nango.ActionError({
             message: 'Invalid input provided to delete a user'
         });
     }
 
+    const foundUserId = await fetchUserIdByEmail(nango, input.email);
+
+    if (!foundUserId) {
+        throw new nango.ActionError({
+            message: 'User with the provided email not found'
+        });
+    }
+
     const config: ProxyConfiguration = {
         // https://developer.freshdesk.com/api/#delete_agent
-        endpoint: `/api/v2/agents/${parsedInput.data.id}`,
+        endpoint: `/api/v2/agents/${foundUserId}`,
         retries: 10
     };
 
@@ -26,3 +55,4 @@ export default async function runAction(nango: NangoAction, input: IdEntity): Pr
         success: true
     };
 }
+
