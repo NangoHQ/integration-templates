17d15147b40fb590bd03a9292c30a7a142e44c37
mungai <mungaihosea@gmail.com>
2024-11-07 15:48:54 +0300

    Can now remove a user from a calendly membership by email

--- integrations/calendly/actions/delete-user.ts
+++ integrations/calendly/actions/delete-user.ts
@@ -1,32 +1,60 @@
-import type { NangoAction, ProxyConfiguration, SuccessResponse, IdEntity } from '../../models';
-import { idEntitySchema } from '../schema.zod.js';
+import type { NangoAction, ProxyConfiguration, SuccessResponse, EmailEntity } from '../../models';
+import { getOrganizationId } from '../helpers/get-organization-id';
+import { emailEntitySchema } from '../../../../box/schema.zod.js';
+
+async function getUserIdByEmail(nango: NangoAction, organizationId: string, email: string): Promise<string> {
+    const getMembershipConfig: ProxyConfiguration = {
+        endpoint: `/organization_memberships`,
+        params: {
+            organization: `https://api.calendly.com/organizations/${organizationId}`,
+            email: email
+        },
+        paginate: {
+            response_path: 'collection',
+            limit_name_in_request: 'count',
+            limit: 100
+        }
+    };
+
+    for await (const orgMemberships of nango.paginate(getMembershipConfig)) {
+        const membership = orgMemberships[0];
+        if (membership) {
+            return membership.uri.split('/').pop()!;
+        }
+    }
+
+    throw new nango.ActionError({
+        message: `No user found with email ${email}`
+    });
+}
 
 /**
  * Executes the delete user action by validating input, constructing the endpoint,
  * and making the API call to Calendly to delete the user from an organization.
  */
-export default async function runAction(nango: NangoAction, input: IdEntity): Promise<SuccessResponse> {
-    const parsedInput = idEntitySchema.safeParse(input);
+export default async function runAction(nango: NangoAction, input: EmailEntity): Promise<SuccessResponse> {
+    const parsedInput = emailEntitySchema.safeParse(input);
 
     if (!parsedInput.success) {
-        for (const error of parsedInput.error.errors) {
-            await nango.log(`Invalid input provided to delete a user: ${error.message} at path ${error.path.join('.')}`, { level: 'error' });
-        }
-
         throw new nango.ActionError({
             message: 'Invalid input provided to delete a user'
         });
     }
 
-    const config: ProxyConfiguration = {
-        // https://developer.calendly.com/api-docs/269e89d9f559f-remove-user-from-organization
-        endpoint: `/organization_memberships/${parsedInput.data.id}`,
+    const organizationId = await getOrganizationId(nango);
+    const userId = await getUserIdByEmail(nango, organizationId, input.email);
+
+    await nango.log(`Deleting user ${userId} from organization ${organizationId}`);
+
+    const deleteUserConfig: ProxyConfiguration = {
+        endpoint: `/organization_memberships/${userId}`,
         retries: 10
     };
 
-    await nango.delete(config);
+    await nango.delete(deleteUserConfig);
 
     return {
         success: true
     };
 }
+
