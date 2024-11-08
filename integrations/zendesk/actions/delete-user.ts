33d3a85684b651bf8595bc75ed55ed99d27e6b95
mungai <mungaihosea@gmail.com>
2024-11-08 12:44:13 +0300

    Added a user to delete a user by email on zendesk

--- integrations/zendesk/actions/delete-user.ts
+++ integrations/zendesk/actions/delete-user.ts
@@ -1,25 +1,72 @@
-import type { NangoAction, ProxyConfiguration, SuccessResponse, IdEntity } from '../../models';
+import type { NangoAction, ProxyConfiguration, SuccessResponse, EmailEntity } from '../../models';
 import { getSubdomain } from '../helpers/get-subdomain.js';
+import { ZendeskUser } from '../types';
+import { emailEntitySchema } from '../schema.zod';
 
-export default async function runAction(nango: NangoAction, input: IdEntity): Promise<SuccessResponse> {
-    if (!input.id) {
+
+export default async function runAction(nango: NangoAction, input: EmailEntity): Promise<SuccessResponse> {
+
+    const parsedInput = emailEntitySchema.safeParse(input);
+    if (!parsedInput.success) {
         throw new nango.ActionError({
-            message: 'Id is required to delete a user'
+            message: 'Invalid input provided to delete a user'
         });
     }
 
     const subdomain = await getSubdomain(nango);
 
-    const config: ProxyConfiguration = {
-        baseUrlOverride: `https://${subdomain}.zendesk.com`,
+    const foundUserId = await findUserByEmail(nango, subdomain, input.email);
+
+    if (!foundUserId) {
+        throw new nango.ActionError({   
+            message: `No user found with email ${input.email}`
+        });
+    }
+
+    const deleteConfig: ProxyConfiguration = {
         // https://developer.zendesk.com/api-reference/ticketing/users/users/#delete-user
-        endpoint: `/api/v2/users/${input.id}`,
+        baseUrlOverride: `https://${subdomain}.zendesk.com`,
+        endpoint: `/api/v2/users/${foundUserId}`,
         retries: 10
     };
 
-    await nango.delete(config);
+    await nango.delete(deleteConfig);
 
     return {
         success: true
     };
+}
+
+/**
+ * Finds a Zendesk user by their email address
+ * @param nango - The NangoAction instance for making API calls
+ * @param subdomain - The Zendesk subdomain to use in the API URL
+ * @param email - The email address to search for
+ * @returns The user ID if found, null otherwise
+ */
+async function findUserByEmail(nango: NangoAction, subdomain: string|undefined, email: string): Promise<string | null> {
+
+    const roles = ['agent', 'admin'];
+
+    const getUserConfig: ProxyConfiguration = {
+        // https://developer.zendesk.com/api-reference/ticketing/users/users/#list-users
+        baseUrlOverride: `https://${subdomain}.zendesk.com`,
+        endpoint: `/api/v2/users`,
+        retries: 10,
+        params: {
+            roles: roles.join(',')
+        },
+        paginate: {
+            response_path: 'users'
+        }
+    };
+
+    for await (const zUsers of nango.paginate<ZendeskUser>(getUserConfig)) {
+        const matchingUser = zUsers.find((zUser: ZendeskUser) => zUser.email === email);
+        if (matchingUser) {
+            return matchingUser.id.toString();
+        }
+    }
+
+    return null;
 }
