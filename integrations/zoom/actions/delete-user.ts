472b1aee0e1b0a09ed96780550c9dbe841679c31
mungai <mungaihosea@gmail.com>
2024-11-08 12:58:51 +0300

    Added feature to remove user by email from zoom

--- integrations/zoom/actions/delete-user.ts
+++ integrations/zoom/actions/delete-user.ts
@@ -1,19 +1,37 @@
-import type { NangoAction, ProxyConfiguration, SuccessResponse, IdEntity } from '../../models';
+import type { NangoAction, ProxyConfiguration, SuccessResponse, EmailEntity } from '../../models';
+import { emailEntitySchema } from '../schema.zod';
+import { ZoomUser } from '../types';
 
-export default async function runAction(nango: NangoAction, input: IdEntity): Promise<SuccessResponse> {
-    if (!input.id) {
+export default async function runAction(nango: NangoAction, input: EmailEntity): Promise<SuccessResponse> {
+
+    const parsedInput = emailEntitySchema.safeParse(input);
+    if (!parsedInput.success) {
         throw new nango.ActionError({
-            message: 'Id is required to delete a user'
+            message: 'Invalid input provided to delete a user'
         });
     }
 
-    const config: ProxyConfiguration = {
-        // https://developers.zoom.us/docs/api/rest/reference/user/methods/#operation/userDelete
-        endpoint: `/users/${input.id}`,
+    const getUserConfig: ProxyConfiguration = {
+        // https://developers.zoom.us/docs/api/rest/reference/user/methods/#operation/users
+        endpoint: `users/${input.email}`,
         retries: 10
     };
 
-    await nango.delete(config);
+    const { data: user } = await nango.get<ZoomUser>(getUserConfig);
+    
+    if (!user) {
+        throw new nango.ActionError({
+            message: `No user found with email ${input.email}`
+        });
+    }
+
+    const deleteUserConfig: ProxyConfiguration = {
+        // https://developers.zoom.us/docs/api/rest/reference/user/methods/#operation/userDelete
+        endpoint: `/users/${user.id}`,
+        retries: 10
+    };
+
+    await nango.delete(deleteUserConfig);
 
     return {
         success: true
