7ff9ec1d7007ba096bef4c6df99be63dc216b22b
Daniel Lwetabe <dannylwe11@gmail.com>
2024-11-11 14:43:51 +0300

    fix linting

--- integrations/linkedin/types.ts
+++ integrations/linkedin/types.ts
@@ -1,45 +1,45 @@
 export interface LinkedInProfileRespose {
     initializeUploadRequest: {
-        owner: string,
-        fileSizeBytes: number ,
-        uploadCaptions: boolean,
-        uploadThumbnail: boolean
+        owner: string;
+        fileSizeBytes: number;
+        uploadCaptions: boolean;
+        uploadThumbnail: boolean;
     }
 }
 
 export interface LinkedInInitializeVideoUploadResponse {
     value: {
-        uploadUrlsExpireAt: number,
-        video: string,
-        uploadInstructions: uploadParams[],
-        uploadToken: string
+        uploadUrlsExpireAt: number;
+        video: string;
+        uploadInstructions: uploadParams[];
+        uploadToken: string;
     }
 }
 
 export interface uploadParams {
-    uploadUrl: string,
-    lastByte: number,
-    firstByte: number
+    uploadUrl: string;
+    lastByte: number;
+    firstByte: number;
 }
 
-// can be used for video, document or images. 
+// can be used for video document or images. 
 export interface LinkedinCreatePostWithVideo {
-    author: string,
-    commentary: string,
-    visibility: LinkedinVisibility,
+    author: string;
+    commentary: string;
+    visibility: LinkedinVisibility;
     distribution: {
-        feedDistribution: LinkedinFeed,
-        targetEntities: any[],
-        thirdPartyDistributionChannels: any[]
-    },
+        feedDistribution: LinkedinFeed;
+        targetEntities: any[];
+        thirdPartyDistributionChannels: any[];
+    }
     content: {
         media: {
-            title:string,
-            id: string
+            title:string;
+            id: string;
         }
-    },
-    lifecycleState: PostLifeCycle,
-    isReshareDisabledByAuthor: boolean
+    }
+    lifecycleState: PostLifeCycle;
+    isReshareDisabledByAuthor: boolean;
 }
 
 type PostLifeCycle = "PUBLISHED";
