1d44fff629df827b42302a532cc69864319f350a
Khaliq <khaliq@nango.dev>
2024-11-12 23:18:55 +0200

    fix(intercom): update syncs and actions (#106)
    ## Describe your changes
    Clean up intercom syncs and actions
    
    ## Issue ticket number and link
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    - [x] I added tests, otherwise the reason is:
    - [ ] External API requests have `retries`
    - [x] Pagination is used where appropriate
    - [x] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [ ] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    - [ ] If the sync is a `full` sync then `track_deletes: true` is set
    - [ ] I followed the best practices and guidelines from the [Writing
    Integration
    Scripts](/NangoHQ/integration-templates/blob/main/WRITING_INTEGRATION_SCRIPTS.md)
    doc

--- integrations/intercom/mappers/to-user.ts
+++ integrations/intercom/mappers/to-user.ts
@@ -1,5 +1,5 @@
 import type { User } from '../../models';
-import type { IntercomContact } from '../types';
+import type { IntercomContact, IntercomAdminUser } from '../types';
 
 /**
  * Maps an Intercom API contact object to a Nango User object.
@@ -7,7 +7,7 @@ import type { IntercomContact } from '../types';
  * @param contact The raw contact object from the Intercom API.
  * @returns Mapped User object with essential properties.
  */
-export function toUser(contact: IntercomContact): User {
+export function toUser(contact: IntercomContact | IntercomAdminUser): User {
     const [firstName = '', lastName = ''] = (contact?.name ?? '').split(' ');
 
     return {
