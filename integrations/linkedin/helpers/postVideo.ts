604cffe945aefe2835df5403949ccc16246e2d48
Daniel Lwetabe <dannylwe11@gmail.com>
2024-11-18 09:00:35 +0300

    fix linting

--- integrations/linkedin/helpers/postVideo.ts
+++ integrations/linkedin/helpers/postVideo.ts
@@ -1,32 +1,32 @@
-import { NangoAction, CreateLinkedInPostWithVideoResponse, ProxyConfiguration } from "../../models";
+import { NangoAction, CreateLinkedInPostWithVideoResponse, ProxyConfiguration } from '../../models';
 import { LinkedinCreatePost } from '../types.js';
 
 export async function createPostWithVideo(
-    nango: NangoAction, 
+    nango: NangoAction,
     author: string,
     postText: string,
     videoTitle: string,
-    videoURN: string): 
-    Promise<CreateLinkedInPostWithVideoResponse> {
+    videoURN: string
+): Promise<CreateLinkedInPostWithVideoResponse> {
     const postData: LinkedinCreatePost = {
         author: `urn:li:person:${author}`,
         commentary: postText,
         visibility: 'PUBLIC',
         distribution: {
-            feedDistribution: "MAIN_FEED",
+            feedDistribution: 'MAIN_FEED',
             targetEntities: [],
             thirdPartyDistributionChannels: []
         },
         content: {
             media: {
-            title: videoTitle,
-            // video that is already uploaded to linkedin api. this id can be video, image or document urn.
-            id: videoURN
+                title: videoTitle,
+                // video that is already uploaded to linkedin api. this id can be video, image or document urn.
+                id: videoURN
             }
         },
         lifecycleState: 'PUBLISHED',
         isReshareDisabledByAuthor: false
-    }
+    };
 
     const config: ProxyConfiguration = {
         // https://learn.microsoft.com/en-us/linkedin/marketing/community-management/shares/posts-api?view=li-lms-2024-10&tabs=http        endpoint: `/rest/posts`,
@@ -35,9 +35,9 @@ export async function createPostWithVideo(
         endpoint: `/rest/posts`,
         headers: {
             'LinkedIn-Version': '202405',
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         }
-    }
+    };
 
     const response = await nango.post(config);
 
@@ -48,6 +48,6 @@ export async function createPostWithVideo(
     }
 
     return {
-        succcess: response.status == 200 ? true : false,
-    }
+        succcess: response.status == 200 ? true : false
+    };
 }
