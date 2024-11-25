a990b3d2bb617ce852a9ec9fcfe5d6d001973584
Daniel Lwetabe <dannylwe11@gmail.com>
2024-11-25 10:56:58 +0300

    remove unused

--- integrations/lever-basic/actions/create-post.ts
+++
@@ -1,5 +0,0 @@
-import type { NangoAction, SuccessResponse, Anonymous_leverbasic_action_createpost_input } from '../../models';
-
-export default async function runAction(nango: NangoAction, input: Anonymous_leverbasic_action_createpost_input): Promise<SuccessResponse> {
-    // Integration code goes here
-}
