a2d7e26870d928648cc31c1bc2912c0dd85d9064
Daniel Lwetabe <dannylwe11@gmail.com>
2024-11-29 20:29:45 +0300

    simplify interview stage action

--- integrations/ashby/actions/interview-stage.ts
+++ integrations/ashby/actions/interview-stage.ts
@@ -3,10 +3,13 @@ import type { PaginationParams } from '../helpers/pagination.js';
 import paginate from '../helpers/pagination.js';
 
 export default async function runAction(nango: NangoAction, input: InterviewStageList): Promise<StagesResponse> {
-    return saveAllStages(nango, input.interviewPlanId);
-}
+    if (!input.interviewPlanId) {
+        throw new nango.ActionError({
+            message: 'interviewPlanId is a required field'
+        });
+    }
 
-async function saveAllStages(nango: NangoAction, interviewPlanId: string) {
+    const interviewPlanId = input.interviewPlanId;
     const stageArr: InterviewStageListResponse[] = [];
     const endpoint = `/interviewStage.list`;
     const nextCursor: string | null = null;
