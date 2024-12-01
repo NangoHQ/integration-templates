4db8d289a6acf6e1a91e2697ddac8945aeca3cd7
Daniel Lwetabe <dannylwe11@gmail.com>
2024-12-01 23:51:54 +0300

    refactor create invoice (1)

--- integrations/pennylane/actions/create-invoice.ts
+++ integrations/pennylane/actions/create-invoice.ts
@@ -1,44 +1,8 @@
 import type { CreateInvoice, InvoiceResponse, NangoAction, PennylaneSuccessResponse, ProxyConfiguration } from '../../models.js';
-import { createInvoice } from '../helpers.js';
+import { createInvoice, validateInput } from '../helpers.js';
 
 export default async function runAction(nango: NangoAction, input: CreateInvoice): Promise<PennylaneSuccessResponse> {
-    if (!input.date) {
-        throw new nango.ActionError({
-            message: 'date is a required field'
-        });
-    }
-
-    if (!input.deadline) {
-        throw new nango.ActionError({
-            message: 'deadline is a required field'
-        });
-    }
-
-    if (!input.customer_source_id) {
-        throw new nango.ActionError({
-            message: 'customer source_id is a required field'
-        });
-    }
-
-    if (input.transactions_reference) {
-        if (
-            !input.transactions_reference.banking_provider ||
-            !input.transactions_reference.provider_field_name ||
-            !input.transactions_reference.provider_field_name
-        ) {
-            throw new nango.ActionError({
-                message: 'banking_provider, provider_field_name and provider_field_name are required fields for transactions_reference'
-            });
-        }
-    }
-
-    if (input.imputation_dates) {
-        if (!input.imputation_dates.start_date || !input.imputation_dates.end_date) {
-            throw new nango.ActionError({
-                message: 'start_date, end_date are required fields for imputation_dates'
-            });
-        }
-    }
+    validateInput(input, nango);
 
     if (input.language && !['fr_FR, en_GB'].includes(input.language)) {
         input = { ...input, language: 'en_GB' };
--- integrations/pennylane/helpers.ts
+++ integrations/pennylane/helpers.ts
@@ -1,4 +1,12 @@
-import type { CreateInvoice, CreateSupplier, PennylaneCustomer, PennylaneIndividualCustomer, PennylaneInvoice, PennylaneSupplier } from '../models.js';
+import type {
+    CreateInvoice,
+    CreateSupplier,
+    NangoAction,
+    PennylaneCustomer,
+    PennylaneIndividualCustomer,
+    PennylaneInvoice,
+    PennylaneSupplier
+} from '../models.js';
 
 export function toInvoice(invoice: PennylaneInvoice): PennylaneInvoice {
     return {
@@ -122,4 +130,44 @@ export function createInvoice(input: CreateInvoice) {
                 })
         }
     };
+}
+
+export function validateInput(input: CreateInvoice, nango: NangoAction) {
+    if (!input.date) {
+        throw new nango.ActionError({
+            message: 'date is a required field'
+        });
+    }
+
+    if (!input.deadline) {
+        throw new nango.ActionError({
+            message: 'deadline is a required field'
+        });
+    }
+
+    if (!input.customer_source_id) {
+        throw new nango.ActionError({
+            message: 'customer source_id is a required field'
+        });
+    }
+
+    if (input.transactions_reference) {
+        if (
+            !input.transactions_reference.banking_provider ||
+            !input.transactions_reference.provider_field_name ||
+            !input.transactions_reference.provider_field_name
+        ) {
+            throw new nango.ActionError({
+                message: 'banking_provider, provider_field_name and provider_field_name are required fields for transactions_reference'
+            });
+        }
+    }
+
+    if (input.imputation_dates) {
+        if (!input.imputation_dates.start_date || !input.imputation_dates.end_date) {
+            throw new nango.ActionError({
+                message: 'start_date, end_date are required fields for imputation_dates'
+            });
+        }
+    }
 }
