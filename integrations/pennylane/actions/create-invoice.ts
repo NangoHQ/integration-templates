f0ce96dedd9f2274e41608c7f61f010817648940
Daniel Lwetabe <dannylwe11@gmail.com>
2024-12-01 23:40:45 +0300

    update create invoice

--- integrations/pennylane/actions/create-invoice.ts
+++ integrations/pennylane/actions/create-invoice.ts
@@ -19,6 +19,26 @@ export default async function runAction(nango: NangoAction, input: CreateInvoice
         });
     }
 
+    if (input.transactions_reference) {
+        if (
+            !input.transactions_reference.banking_provider ||
+            !input.transactions_reference.provider_field_name ||
+            !input.transactions_reference.provider_field_name
+        ) {
+            throw new nango.ActionError({
+                message: 'banking_provider, provider_field_name and provider_field_name are required fields for transactions_reference'
+            });
+        }
+    }
+
+    if (input.imputation_dates) {
+        if (!input.imputation_dates.start_date || !input.imputation_dates.end_date) {
+            throw new nango.ActionError({
+                message: 'start_date, end_date are required fields for imputation_dates'
+            });
+        }
+    }
+
     if (input.language && !['fr_FR, en_GB'].includes(input.language)) {
         input = { ...input, language: 'en_GB' };
     }
