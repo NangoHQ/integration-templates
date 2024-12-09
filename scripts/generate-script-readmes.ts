aa5067b89ea82d334ad0d6f3e1b843811ed88fa1
Alan Johnson <alan@nango.dev>
2024-12-09 12:03:25 -0500

    Fix up parameter handling

--- scripts/generate-script-readmes.ts
+++ scripts/generate-script-readmes.ts
@@ -68,6 +68,7 @@ function updateReadme(sections: MarkdownSections, scriptName: string, scriptPath
     sections = updateGeneralInfo(sections, scriptPath, endpointType, scriptConfig);
     sections = updateSection(sections, '## Endpoint Reference', [], 2);
     sections = updateRequestEndpoint(sections, scriptConfig);
+    sections = updateRequestParams(sections, scriptConfig, endpointType);
     sections = updateRequestBody(sections, scriptConfig, endpointType);
     return sections;
 }
@@ -138,9 +139,26 @@ function updateRequestEndpoint(sections: MarkdownSections, scriptConfig: any) {
     return updateSection(sections, title, content, 3);
 }
 
+function updateRequestParams(sections: MarkdownSections, scriptConfig: any, endpointType: string) {
+    const title = '### Request Query Parameters';
+    const content =
+        endpointType === 'action'
+            ? [``, `_No request parameters_`, ``]
+            : [
+                  ``,
+                  `- **modified_after:** \`(optional, string)\` A timestamp (e.g., \`2023-05-31T11:46:13.390Z\`) used to fetch records modified after this date and time. If not provided, all records are returned. The modified_after parameter is less precise than cursor, as multiple records may share the same modification timestamp.`,
+                  `- **limit:** \`(optional, integer)\` The maximum number of records to return per page. Defaults to 100.`,
+                  `- **cursor:** \`(optional, string)\` A marker used to fetch records modified after a specific point in time.If not provided, all records are returned.Each record includes a cursor value found in _nango_metadata.cursor.Save the cursor from the last record retrieved to track your sync progress.Use the cursor parameter together with the limit parameter to paginate through records.The cursor is more precise than modified_after, as it can differentiate between records with the same modification timestamp.`,
+                  `- **filter:** \`(optional, added | updated | deleted)\` Filter to only show results that have been added or updated or deleted.`,
+                  ``
+              ];
+
+    return updateSection(sections, title, content, 4);
+}
+
 function updateRequestBody(sections: MarkdownSections, scriptConfig: any, endpointType: string) {
     const title = '### Request Body';
     const content = [``, `\`\`\`json`, `Hello World`, `\`\`\``, ``];
 
-    return updateSection(sections, title, content, 4);
+    return updateSection(sections, title, content, 5);
 }
