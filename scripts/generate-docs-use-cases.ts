390326aea66a79ca6713d81cf0359480b170d542
nalanj <5594+nalanj@users.noreply.github.com>
2024-12-11 13:46:04 -0500

    feat: Add script for dumping use-cases to disk for documentation (#142)
    ## Describe your changes
    
    Adds a new script for dumping the data we need for use-cases in the repo
    to disk. A near-term PR will support running it alongside our flows.yaml
    sync.
    
    ## Issue ticket number and link
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    
    -   [ ] I added tests, otherwise the reason is:
    -   [ ] External API requests have `retries`
    -   [ ] Pagination is used where appropriate
    - [ ] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [ ] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    -   [ ] If the sync is a `full` sync then `track_deletes: true` is set
    - [ ] I followed the best practices and guidelines from the [Writing
    Integration
    Scripts](/NangoHQ/integration-templates/blob/main/WRITING_INTEGRATION_SCRIPTS.md)
    doc

---
+++ scripts/generate-docs-use-cases.ts
@@ -0,0 +1,45 @@
+import { promises as fs } from 'fs';
+import yaml from 'js-yaml';
+
+interface UseCase {
+    method: string;
+    path: string;
+    description: string;
+    group: string;
+    script: string;
+}
+
+const maybeIntegrations = await fs.readdir('integrations', { withFileTypes: true });
+const integrations = maybeIntegrations.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
+
+const exportData: Record<string, UseCase[]> = {};
+
+for (const integration of integrations) {
+    // eslint-disable-next-line @nangohq/custom-integrations-linting/no-object-casting
+    const yamlConfig = yaml.load(await fs.readFile(`integrations/${integration}/nango.yaml`, 'utf8')) as any;
+    const config = yamlConfig.integrations[integration] || yamlConfig.integrations['${PWD}'];
+
+    exportData[integration] = readUseCases(config.syncs, integration).concat(readUseCases(config.actions, integration));
+}
+
+await fs.writeFile('use-cases.json', JSON.stringify(exportData, null, 2), 'utf-8');
+
+function readUseCases(syncOrAction: any, integration: string) {
+    const endpoints: UseCase[] = [];
+    if (syncOrAction) {
+        for (const [key, item] of Object.entries<any>(syncOrAction)) {
+            const syncEndpoints = Array.isArray(item.endpoint) ? item.endpoint : [item.endpoint];
+            for (const endpoint of syncEndpoints) {
+                endpoints.push({
+                    method: endpoint.method,
+                    path: endpoint.path,
+                    description: item.description?.trim(),
+                    group: endpoint.group,
+                    script: `${integration}/actions/${key}`
+                });
+            }
+        }
+    }
+
+    return endpoints;
+}
