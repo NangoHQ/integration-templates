14b3ef23b2173d63f24a92926dc89aa2ba75740a
Daniel Lwetabe <dannylwe11@gmail.com>
2024-12-13 23:11:17 +0300

    [ext-256] migrate linkedin logic from helper (1)

--- integrations/linkedin/actions/post.ts
+++ integrations/linkedin/actions/post.ts
@@ -1,6 +1,6 @@
-import type { CreateLinkedInPostWithVideoResponse, NangoAction, LinkedinVideoPost } from '../../models';
-import { createPostWithVideo } from '../helpers/post-video.js';
+import type { CreateLinkedInPostWithVideoResponse, LinkedinVideoPost, NangoAction, ProxyConfiguration } from '../../models';
 import { userInfo } from '../helpers/user-info.js';
+import type { LinkedinCreatePost } from '../types';
 
 export default async function runAction(nango: NangoAction, input: LinkedinVideoPost): Promise<CreateLinkedInPostWithVideoResponse> {
     const videoURN = input?.videoURN;
@@ -17,7 +17,48 @@ export default async function runAction(nango: NangoAction, input: LinkedinVideo
         });
     }
 
-    const resp = await createPostWithVideo(nango, ownerId, input.text, input.videoTitle, videoURN);
+    const postData: LinkedinCreatePost = {
+        author: `urn:li:person:${ownerId}`,
+        commentary: input.postText,
+        visibility: 'PUBLIC',
+        distribution: {
+            feedDistribution: 'MAIN_FEED',
+            targetEntities: [],
+            thirdPartyDistributionChannels: []
+        },
+        lifecycleState: 'PUBLISHED',
+        isReshareDisabledByAuthor: false
+    };
 
-    return resp;
+    if (videoURN) {
+        postData.content = {
+            media: {
+                title: input.videoTitle,
+                // video that is already uploaded to linkedin api. this id can be video, image or document urn.
+                id: videoURN
+            }
+        };
+    }
+
+    const config: ProxyConfiguration = {
+        // https://learn.microsoft.com/en-us/linkedin/marketing/community-management/shares/posts-api?view=li-lms-2024-10&tabs=http
+        endpoint: `/rest/posts`,
+        retries: 10,
+        data: postData,
+        headers: {
+            'LinkedIn-Version': '202405'
+        }
+    };
+
+    const response = await nango.post(config);
+
+    if (response.status !== 200) {
+        throw new nango.ActionError({
+            message: `failed to create post with video urn ${videoURN}`
+        });
+    }
+
+    return {
+        succcess: response.status == 200 ? true : false
+    };
 }
