81e2e9a6f7c473a55a6d815cb6d76cb23940955f
AndrewKaranja <andywanji56@gmail.com>
2025-01-08 13:43:41 +0300

    fix(metabase): fixed the response path

--- integrations/metabase/syncs/list-users.ts
+++ integrations/metabase/syncs/list-users.ts
@@ -1,16 +1,24 @@
 import type { NangoSync, ProxyConfiguration } from '../../models';
-import { UserSchema } from '../schema.zod.js';
 
 export default async function fetchData(nango: NangoSync): Promise<void> {
     const config: ProxyConfiguration = {
         // https://www.metabase.com/docs/latest/api-documentation.html#get-api-user
         endpoint: '/api/user',
         retries: 10,
-        paginate: { response_path: 'results', limit: 100, type: 'offset', offset_name_in_request: 'offset', limit_name_in_request: 'limit' }
+        paginate: { response_path: 'data', limit: 100, type: 'offset', offset_name_in_request: 'offset', limit_name_in_request: 'limit' }
     };
 
     for await (const page of nango.paginate(config)) {
-        const validatedUsers = page.map((user) => UserSchema.parse(user));
+        const validatedUsers = page.map((user) => {
+            return {
+                id: user.id.toString(),
+                firstName: user.first_name,
+                lastName: user.last_name,
+                email: user.email
+            };
+        });
+        await nango.log('111');
+        await nango.log(validatedUsers);
         await nango.batchSave(validatedUsers, 'User');
     }
 }
