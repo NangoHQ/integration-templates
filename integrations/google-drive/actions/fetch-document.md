14100f321480ce03e0e38b1d4fef8ea3c4cae390
Khaliq <khaliqgant@gmail.com>
2025-02-14 14:04:35 +0200

    update actions

--- integrations/google-drive/actions/fetch-document.md
+++ integrations/google-drive/actions/fetch-document.md
@@ -6,8 +6,10 @@
 - **Description:** Fetches the content of a file given its ID, processes the data using
 a response stream, and encodes it into a base64 string. This base64-encoded
 string can be used to recreate the file in its original format using an external tool.
+If this is a native google file type then use the fetch-google-sheet or fetch-google-doc
+actions.
 
-- **Version:** 2.0.0
+- **Version:** 2.0.1
 - **Group:** Documents
 - **Scopes:** `https://www.googleapis.com/auth/drive.readonly`
 - **Endpoint Type:** Action
---
+++ integrations/google-drive/actions/fetch-google-doc.ts
@@ -0,0 +1,28 @@
+import type { NangoAction, ProxyConfiguration, IdEntity, JSONDocument } from '../../models';
+
+export default async function runAction(nango: NangoAction, input: IdEntity): Promise<JSONDocument> {
+    if (!input || !input.id) {
+        throw new nango.ActionError({
+            message: 'Invalid input',
+            details: 'The input must be an object with an "id" property.'
+        });
+    }
+
+    const config: ProxyConfiguration = {
+        baseUrlOverride: 'https://docs.googleapis.com',
+        // https://developers.google.com/docs/api/reference/rest/v1/documents/get
+        endpoint: `/v1/documents/${input.id}`,
+        params: {
+            includeTabsContent: 'true'
+        },
+        retries: 10
+    };
+
+    const documentResponse = await nango.get<JSONDocument>(config);
+
+    if (documentResponse.status !== 200) {
+        throw new nango.ActionError(`Failed to fetch document: Status Code ${documentResponse.status}`);
+    }
+
+    return documentResponse.data;
+}
