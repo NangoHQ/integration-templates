2622d31d68264ba59b69ecf0c3cfaf2cc069c720
Hassan_Wari <thebigstormtech@gmail.com>
2025-02-20 13:32:01 +0300

    feat(shopify): orders sync

---
+++ integrations/shopify/helpers/paginate.ts
@@ -0,0 +1,84 @@
+import type { NangoSync, ProxyConfiguration } from '../../models.js';
+import { buildGraphQLQuery } from './query-builder.js';
+import type { ShopifyPaginationParams, ShopifyResponse, PageInfo } from '../types';
+
+const BATCH_SIZE = 1;
+
+export async function* paginate(nango: NangoSync, tableName: string, topLevelFields: string[], paginatedFields: { field: string; fields: string[] }[]) {
+    let cursor: string | null = null;
+    let hasNextPage = true;
+    let batch: any[] = [];
+
+    const lastSyncDate = nango.lastSyncDate || undefined;
+
+    do {
+        const variables: ShopifyPaginationParams = { first: 1, after: cursor };
+        for (const { field } of paginatedFields) {
+            variables[`${field}After`] = null;
+            variables[`${field}First`] = 1;
+        }
+
+        const query = buildGraphQLQuery(tableName, topLevelFields, paginatedFields, lastSyncDate);
+        const config: ProxyConfiguration = {
+            // https://shopify.dev/docs/api/admin-graphql
+            endpoint: `/admin/api/2024-01/graphql.json`,
+            data: { query, variables },
+            retries: 10
+        };
+
+        const response = await nango.post<ShopifyResponse>(config);
+
+        if (response.data.errors) {
+            throw new Error(JSON.stringify(response.data.errors, null, 2));
+        }
+
+        const edges = response.data['data']?.[tableName]?.edges || [];
+        const pageInfo: PageInfo = response.data['data']?.[tableName]?.pageInfo ?? { hasNextPage: false, endCursor: null };
+
+        hasNextPage = pageInfo.hasNextPage;
+
+        for (const edge of edges) {
+            const node = edge.node;
+            for (const { field } of paginatedFields) {
+                let nestedCursor: string | null = node[field]?.pageInfo?.endCursor || null;
+                let nestedHasNextPage = node[field]?.pageInfo?.hasNextPage || false;
+                node[field] = node[field]?.edges?.map((e: any) => e.node) || [];
+
+                while (nestedHasNextPage) {
+                    variables[`${field}After`] = nestedCursor;
+                    const nestedQuery = buildGraphQLQuery(tableName, topLevelFields, paginatedFields, lastSyncDate);
+                    const nestedConfig: ProxyConfiguration = {
+                        // https://shopify.dev/docs/api/admin-graphql
+                        endpoint: `/admin/api/2024-01/graphql.json`,
+                        data: { query: nestedQuery, variables },
+                        retries: 10
+                    };
+
+                    const nestedResponse = await nango.post<ShopifyResponse>(nestedConfig);
+
+                    if (nestedResponse.data.errors) {
+                        throw new Error(JSON.stringify(nestedResponse.data.errors, null, 2));
+                    }
+
+                    const nestedEdges = nestedResponse.data['data']?.[tableName]?.edges || [];
+                    node[field].push(...nestedEdges.flatMap((e: any) => e.node[field]?.edges.map((edge: any) => edge.node) || []));
+
+                    const newNestedPageInfo = nestedEdges.length > 0 ? nestedEdges[nestedEdges.length - 1]?.node?.[field]?.pageInfo : {};
+                    nestedHasNextPage = newNestedPageInfo.hasNextPage || false;
+                    nestedCursor = newNestedPageInfo.endCursor || null;
+                }
+            }
+
+            batch.push(node);
+
+            if (batch.length >= BATCH_SIZE) {
+                yield batch;
+                batch = [];
+            }
+        }
+        cursor = pageInfo.endCursor;
+    } while (hasNextPage);
+    if (batch.length > 0) {
+        yield batch;
+    }
+}
---
+++ integrations/shopify/types.ts
@@ -0,0 +1,77 @@
+export interface ShopifyPaginationParams {
+    first: number;
+    after: string | null;
+    lastSyncDate?: string;
+    [key: `${string}After`]: string | null;
+    [key: `${string}First`]: number;
+}
+
+export interface PageInfo {
+    hasNextPage: boolean;
+    endCursor: string | null;
+}
+
+export interface ShopifyResponse {
+    data: Record<string, ResponseData<any>>;
+    errors?: any;
+}
+
+export interface ResponseData<T> {
+    edges: { node: T; cursor: string }[];
+    pageInfo: PageInfo;
+}
+
+interface MoneySet {
+    presentmentMoney: {
+        amount: string;
+        currencyCode: string;
+    };
+}
+
+interface Customer {
+    firstName: string;
+    lastName: string;
+    displayName: string;
+    email: string | null;
+    phone: string | null;
+}
+
+interface Address {
+    address1: string;
+    address2: string | null;
+    city: string;
+    country: string;
+    province: string | null;
+    zip: string | null;
+}
+
+export interface LineItem {
+    id: string;
+    name: string;
+    quantity: number;
+    originalTotalSet: MoneySet;
+    discountedTotalSet: MoneySet;
+}
+
+export interface ShopifyOrder {
+    id: string;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+    processedAt: string;
+    currencyCode: string;
+    presentmentCurrencyCode: string;
+    confirmed: boolean;
+    cancelledAt: string | null;
+    cancelReason: string | null;
+    closed: boolean;
+    closedAt: string | null;
+    fullyPaid: boolean;
+    customer: Customer | null;
+    totalReceivedSet: MoneySet;
+    subtotalPriceSet: MoneySet;
+    totalTaxSet: MoneySet;
+    shippingAddress: Address | null;
+    billingAddress: Address | null;
+    lineItems: LineItem[];
+}
