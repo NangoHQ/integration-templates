9a101f7434dd1ed4a80fb2ccc358db5897fe19bf
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2025-02-20 17:14:38 +0300

    feat(shopify): orders sync (#227)
    ## Describe your changes
    
    ## Issue ticket number and link
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    
    -   [x] I added tests, otherwise the reason is:
    -   [x] External API requests have `retries`
    -   [ ] Pagination is used where appropriate
    - [ ] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [x] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    -   [ ] If the sync is a `full` sync then `track_deletes: true` is set
    - [x] I followed the best practices and guidelines from the [Writing
    Integration
    Scripts](/NangoHQ/integration-templates/blob/main/WRITING_INTEGRATION_SCRIPTS.md)
    doc
    
    ---------
    
    Co-authored-by: Khaliq <khaliq@nango.dev>

---
+++ integrations/shopify/syncs/orders.ts
@@ -0,0 +1,92 @@
+import type { NangoSync } from '../../models';
+import { paginate } from '../helpers/paginate.js';
+import { toOrder } from '../mappers/to-order.js';
+
+export default async function fetchData(nango: NangoSync): Promise<void> {
+    const tableName = 'orders';
+    const topLevelFields = [
+        'id',
+        'name',
+        'createdAt',
+        'updatedAt',
+        'processedAt',
+        'currencyCode',
+        'presentmentCurrencyCode',
+        'confirmed',
+        'cancelledAt',
+        'cancelReason',
+        'closed',
+        'closedAt',
+        'fullyPaid',
+        `customer {
+                firstName
+                lastName
+                displayName
+                email
+                phone
+            }`,
+        `totalReceivedSet {
+                presentmentMoney {
+                    amount
+                    currencyCode
+                }
+            }`,
+        `subtotalPriceSet {
+                presentmentMoney {
+                    amount
+                    currencyCode
+                }
+            }`,
+        `totalTaxSet {
+                presentmentMoney {
+                    amount
+                    currencyCode
+                }
+            }`,
+        `shippingAddress {
+                address1
+                address2
+                city
+                country
+                province
+                zip
+            }`,
+        `billingAddress {
+                address1
+                address2
+                city
+                country
+                province
+                zip
+            }`
+    ];
+
+    const paginatedFields = [
+        {
+            field: 'lineItems',
+            fields: [
+                'id',
+                'name',
+                'quantity',
+                `originalTotalSet {
+                        presentmentMoney {
+                            amount
+                            currencyCode
+                        }
+                    }`,
+                `discountedTotalSet {
+                        presentmentMoney {
+                            amount
+                            currencyCode
+                        }
+                    }`
+            ]
+        }
+    ];
+
+    for await (const batch of paginate(nango, tableName, topLevelFields, paginatedFields)) {
+        // https://shopify.dev/docs/api/admin-graphql/2025-01/queries/orders
+        const mappedOrders = batch.map(toOrder);
+        await nango.batchSave(mappedOrders, 'Order');
+    }
+}
