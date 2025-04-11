e58380ffe9d6046408142a0910307cb026dd685a
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2025-04-11 20:17:26 +0300

    fix(workday): properly interpolate connectionConfigs (#309)
    ## Describe your changes
    - Properly interpolate connectionConfigs 
    ## Issue ticket number and link
    
    ## Checklist before requesting a review (skip if just adding/editing
    APIs & templates)
    
    -   [ ] I added tests, otherwise the reason is:
    -   [ ] External API requests have `retries`
    -   [ ] Pagination is used where appropriate
    - [ ] The built in `nango.paginate` call is used instead of a `while
    (true)` loop
    - [ ] Third party requests are NOT parallelized (this can cause issues
    with rate limits)
    - [ ] If a sync requires metadata the `nango.yaml` has `auto_start:
    false`
    -   [ ] If the sync is a `full` sync then `track_deletes: true` is set
    - [ ] I followed the best practices and guidelines from the [Writing
    Integration
    Scripts](/NangoHQ/integration-templates/blob/main/guides/WRITING_SCRIPTS.md)
    doc

--- integrations/workday/utils.ts
+++ integrations/workday/utils.ts
@@ -4,22 +4,21 @@ import type { NangoSync } from '../models';
 const version = '42.0';
 
 export async function getSoapClient(type: 'Human_Resources' | 'Staffing', connection: Awaited<ReturnType<NangoSync['getConnection']>>) {
-    if (connection.credentials.type !== 'BASIC' || !connection.credentials.password || !connection.credentials.username) {
-        throw new Error('invalid_credentials');
+    const { credentials, connection_config } = connection;
+
+    if (credentials.type !== 'BASIC' || !credentials.username || !credentials.password || !connection_config['hostname'] || !connection_config['tenant']) {
+        throw new Error('Invalid credentials: BASIC auth, username, password, hostname, and tenant are all required.');
     }
 
     // Create SOAP client
-    const client = await soap.createClientAsync(
-        `https://community.workday.com/sites/default/files/file-hosting/productionapi/${type}/v${version}/${type}.wsdl`,
-        {}
-    );
+    const wsdlUrl = `https://community.workday.com/sites/default/files/file-hosting/productionapi/${type}/v${version}/${type}.wsdl`;
+    const endpointUrl = `https://${connection_config['hostname']}/ccx/service/${connection_config['tenant']}/${type}/v${version}`;
+
+    const client = await soap.createClientAsync(wsdlUrl, {});
     client.addHttpHeader('Accept-Encoding', 'gzip, deflate'); // Needed or some queries will fail https://github.com/axios/axios/issues/4806
 
-    client.setSecurity(new soap.WSSecurity(connection.credentials.username, connection.credentials.password));
-
-    // Each customers has its own endpoint
-    // TODO: parametrize that
-    client.setEndpoint(`https://impl-services1.wd12.myworkday.com/ccx/service/activecybersrv_dpt2/${type}/v${version}`);
+    client.setSecurity(new soap.WSSecurity(credentials.username, credentials.password));
+    client.setEndpoint(endpointUrl);
 
     return client;
 }
