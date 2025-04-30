b518cf69aa6fc23e1afc2396d4642f440265fc2e
lordsarcastic <adeoti.15.jude@gmail.com>
2025-04-30 08:26:10 +0100

    Adjusted retries and validation

--- integrations/grammarly/actions/delete-user.ts
+++ integrations/grammarly/actions/delete-user.ts
@@ -1,16 +1,13 @@
 import type { NangoAction, SuccessResponse, EmailEntity, ProxyConfiguration } from '../../models';
+import { emailEntitySchema } from '../schema.zod';
 
 export default async function runAction(nango: NangoAction, input: EmailEntity): Promise<SuccessResponse> {
-    if (!input || !input.email) {
-        throw new nango.ActionError({
-            message: 'Email is required to delete a user'
-        });
-    }
+    await nango.zodValidateInput({ zodSchema: emailEntitySchema, input });
 
     const config: ProxyConfiguration = {
         // https://developer.grammarly.com/license-management-api.html#remove-the-user-from-the-institution
         endpoint: `/users/${input.email}`,
-        retries: 10
+        retries: 3
     };
 
     await nango.delete(config);
