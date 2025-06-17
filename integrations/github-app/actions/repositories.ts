a564be8f38e6ef0b9fd1ee719ef4b64eb05c4deb
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2025-06-17 19:00:20 +0300

    fix(github-repositiories-action): fix pagination to start from page 1

--- integrations/github-app/actions/repositories.ts
+++ integrations/github-app/actions/repositories.ts
@@ -5,9 +5,11 @@ export default async function runAction(nango: NangoSync): Promise<RepoResponse>
         // https://docs.github.com/en/rest/apps/installations?apiVersion=2022-11-28#list-repositories-accessible-to-the-app-installation
         endpoint: '/installation/repositories',
         paginate: {
+            limit: 100,
             response_path: 'repositories',
             type: 'offset',
             offset_name_in_request: 'page',
+            offset_start_value: 1,
             offset_calculation_method: 'per-page'
         }
     };
