857e3903ee8f2ff7587fd78726702da2c1a2a7cb
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2025-06-26 22:59:49 +0300

    more

--- integrations/outlook/tests/outlook-events.test.ts
+++ integrations/outlook/tests/outlook-events.test.ts
@@ -9,7 +9,7 @@ describe('outlook events tests', () => {
         Model: 'OutlookCalendarEvent'
     });
 
-    vi.setSystemTime('2025-01-01T22:00:00.000Z');
+    vi.setSystemTime('2025-05-27T15:49:10.826Z');
 
     const models = 'OutlookCalendarEvent'.split(',');
     const batchSaveSpy = vi.spyOn(nangoMock, 'batchSave');
