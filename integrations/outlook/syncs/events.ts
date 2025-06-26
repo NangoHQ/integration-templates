d43719e11722164306fe869b95a443b99179bae2
Hassan_Wari <85742599+hassan254-prog@users.noreply.github.com>
2025-06-26 22:43:38 +0300

    fix test

--- integrations/outlook/syncs/events.ts
+++ integrations/outlook/syncs/events.ts
@@ -25,7 +25,7 @@ export default async function fetchData(nango: NangoSync): Promise<void> {
             type: 'link',
             response_path: 'value',
             link_path_in_response_body: '@odata.nextLink',
-            limit: 1,
+            limit: 100,
             limit_name_in_request: '$top'
         },
         retries: 10
