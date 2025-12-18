cf0cc0384604a53bc5d33154873b0b47b7f7b386
Khaliq <khaliqgant@gmail.com>
2025-12-18 12:46:32 +0100

    fix tests

--- integrations/attio/tests/attio-list-objects.test.ts
+++ integrations/attio/tests/attio-list-objects.test.ts
@@ -3,11 +3,11 @@ import { vi, expect, it, describe } from 'vitest';
 import createAction from '../actions/list-objects.js';
 
 describe('attio list-objects tests', () => {
-  const nangoMock = new global.vitest.NangoActionMock({ 
-      dirname: 'attio',
-      name: "list-objects",
-      Model: "ActionOutput_attio_listobjects"
-  });
+    const nangoMock = new global.vitest.NangoActionMock({
+        dirname: __dirname,
+        name: 'list-objects',
+        Model: 'AttioObject'
+    });
 
   it('should output the action output that is expected', async () => {
       const input = await nangoMock.getInput();
