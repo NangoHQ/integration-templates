b4ba22f2c25e03236aabbc5052590229e7cbd832
kaposke <guilhermebassa@gmail.com>
2026-03-05 16:26:37 -0300

    Dont create artifacts per flow

--- internal/scripts/zero/types.ts
+++ internal/scripts/zero/types.ts
@@ -1,3 +1,3 @@
 import type { NangoYamlParsedIntegration } from '@nangohq/types';
 
-export type ZeroFlow = NangoYamlParsedIntegration & { jsonSchema: any; sdkVersion: string; symLinkTargetName: string | null };
+export type ZeroFlow = NangoYamlParsedIntegration & { sdkVersion: string; symLinkTargetName: string | null };
