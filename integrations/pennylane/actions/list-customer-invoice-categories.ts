647ae51def0d3fb02719379ee6265ebe323dee0e
Victor Lang'at <125632594+viictoo@users.noreply.github.com>
2026-07-12 22:35:29 +0300

    feat: bump version - list customer invoice

--- integrations/pennylane/actions/list-customer-invoice-categories.ts
+++ integrations/pennylane/actions/list-customer-invoice-categories.ts
@@ -45,7 +45,7 @@ const OutputSchema = z.object({
 
 const action = createAction({
     description: 'List categories for a customer invoice',
-    version: '1.0.0',
+    version: '1.0.1',
     input: InputSchema,
     output: OutputSchema,
     scopes: ['customer_invoices:all', 'customer_invoices:readonly'],
